563736bd608a2967874f634d4a389ce23624c603
Silvia_Piatino <s268598@studenti.polito.it>
2023-06-10 01:15:01 +0200

    change links

--- download_datasets.py
+++ download_datasets.py
@@ -1,14 +1,18 @@
 
 URLS = {
-    "tokyo_xs": "https://drive.google.com/file/d/1nf1RAas99gNNvoWEJOD8A4knpSsOTr-D/view?usp=sharing",
-    "sf_xs": "https://drive.google.com/file/d/1Rs9cPvGMs5RTuP7uPOaydE52c2dDeOxD/view?usp=sharing",
-    "gsv_xs": "https://drive.google.com/file/d/1RL1Vj9-HgAVWHcSW014OIxpFVJNpkUQr/view?usp=sharing"
+    "tokyo_xs": "https://drive.google.com/file/d/1kHl0xPVWJTNIdJXLOrm3xUS-SL8MvGKO/view?usp=sharing",
+    "sf_xs": "https://drive.google.com/file/d/1OLRHvJqcgruNuVwd_ZVdX7RGvT8DqQQF/view?usp=sharing",
+    "gsv_xs": "https://drive.google.com/file/d/1dnOQhDcT82o_jWLlcO8lS6kaeVdFMzvr/view?usp=sharing"
+
 }
 
-#"tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
+# "tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
 #    "sf_xs": "https://drive.google.com/file/d/1tQqEyt3go3vMh4fj_LZrRcahoTbzzH-y/view?usp=share_link",
 #    "gsv_xs": "https://drive.google.com/file/d/1q7usSe9_5xV5zTfN-1In4DlmF5ReyU_A/view?usp=share_link"
 
+# "tokyo_xs": "https://drive.google.com/file/d/1nf1RAas99gNNvoWEJOD8A4knpSsOTr-D/view?usp=sharing",
+# "sf_xs": "https://drive.google.com/file/d/1Rs9cPvGMs5RTuP7uPOaydE52c2dDeOxD/view?usp=sharing",
+# "gsv_xs": "https://drive.google.com/file/d/1RL1Vj9-HgAVWHcSW014OIxpFVJNpkUQr/view?usp=sharing"
 import os
 import gdown
 import shutil
