1fcf7df94a114774e23d04b7f3f2874b2d11eaa4
Andrea <andrea.tataranni00@gmail.com>
2023-06-15 11:24:29 +0200

    test per cosine annealing

--- main.py
+++ main.py
@@ -62,25 +62,25 @@ class GeoModel(pl.LightningModule):
         return descriptors
 
     #COSINE_ANNEALING
-    # def configure_optimizers(self):
-    #     if args.optimizer == "SGD_cosine":
-    #         optimizers = torch.optim.SGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, momentum=0.9)
-    #     if args.optimizer == "ASGD_cosine":
-    #         optimizers = torch.optim.ASGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
-    #     return {"optimizer": optimizers, "lr_scheduler": {
-    #          "scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(optimizers, 5, eta_min=args.learning_rate*0.01, last_epoch=- 1, verbose=True),
-    #          "frequency": 1 }}
+    def configure_optimizers(self):
+        if args.optimizer == "SGD_cosine":
+            optimizers = torch.optim.SGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, momentum=0.9)
+        if args.optimizer == "ASGD_cosine":
+            optimizers = torch.optim.ASGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+        return {"optimizer": optimizers, "lr_scheduler": {
+             "scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(optimizers, 5, eta_min=args.learning_rate*0.01, last_epoch=- 1, verbose=True),
+             "frequency": 1}}
 
 
     #REDUCE_LR_ON_PLATEAU
-    def configure_optimizers(self):
-        if args.optimizer == "SGD_plateau":
-            optimizers = torch.optim.SGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, momentum=0.9)
-        if args.optimizer == "ASGD_plateau":
-            optimizers = torch.optim.ASGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
-        return {"optimizer": optimizers, "lr_scheduler": {
-            "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(optimizers, mode='min', factor=0.1, verbose=True,
-                                                                    patience=0), "monitor": 'R@1', "frequency": 1}}
+    # def configure_optimizers(self):
+    #     if args.optimizer == "SGD_plateau":
+    #         optimizers = torch.optim.SGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, momentum=0.9)
+    #     if args.optimizer == "ASGD_plateau":
+    #         optimizers = torch.optim.ASGD(self.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    #     return {"optimizer": optimizers, "lr_scheduler": {
+    #         "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(optimizers, mode='min', factor=0.1, verbose=True,
+    #                                                                 patience=0), "monitor": 'R@1', "frequency": 1}}
 
     #NO SCHEDULING
     # def configure_optimizers(self):
